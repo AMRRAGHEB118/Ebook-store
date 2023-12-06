6496ba014371f8c105c4edfc904a7fa693f2fb6d
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-12-07 01:41:27 +0200

    handle add to cart

--- js/bookDetails.js
+++ js/bookDetails.js
@@ -4,127 +4,171 @@ const commentForm = document.getElementById('comment-form');
 const commentInput = document.getElementById('comment-input');
 const urlSearchParams = new URLSearchParams(window.location.search);
 const selectedBook = urlSearchParams.get('id')
+const productListContainer = document.getElementById('productList');
 
 if (!selectedBook) {
-    window.location.href = 'ebooks.html';
+window.location.href = 'ebooks.html';
 }
 
 if (!userDetails) {
-    window.location.href = 'login.html';
+window.location.href = 'login.html';
 } else {
-    if(userDetails.user.role !== 'buyer') {
-        window.location.href = '/';
-    }
+if(userDetails.user.role !== 'buyer') {
+window.location.href = '/';
+}
 }
 
 const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
-    return formattedDate;
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
+return formattedDate;
 };
 
+const addToCart = async (bookId) => {
+const response = await axios.post(`${backendUrl}/cart/${bookId}`, {
+userId: userDetails.user._id,
+quantity: document.getElementById('quantityInput').value
+});
+if (response.data.success) {
+alert('Book added to cart');
+}
+}
+
 const bookDetailsRender = (book) => {
-    bookDetailsContainer.innerHTML = `
-            <div class="row">
-                <div class="col-2">
-                    <img src="${book.coverImage}" alt="Book4" width="68%" />
-                </div>
-                <div class="col-2">
-                    <p>Home / Ebook</p>
-                    <h1>${book.title}</h1>
-                    <h4>$${book.price} <i class="fa-solid fa-tags"></i></h4>
-                    <input id="quantityInput" min="1" type="number" value="1" />
-                    <button data-id="${book._id}" class="btn" onclick="addToCart(${book._id})">Add To Cart</button>
-                    <div class="btnswq">
-                        <button onclick="Toggle1(),show_pup()" id="btnh112" class="btntr"><i class="fas fa-heart"></i></button>
-                        <div class="Cardsa" id="pup">
-                            <img src="../Faver/heart15.png" alt="">
-                            <h1>Add</h1>
-                            <p>To-Favorite</p>
-                            <button class="btncx-close" onclick="hidde_pup()">close</button>
-                        </div>
-                    </div>
-                    <h3>Book Details <i class="fa fa-indent"></i></h3>
-                    <p>${book.description}</p>
-                    <a href="#" class="btn" style="margin-left:100px; margin-top: 65px;">Rating</a>
-                    <form action="">
-                        <div class="rating">
-                            <input type="radio" name="rate" id="rating-op5" data-idx="0" hidden>
-                            <label for="rating-op5"><span>Excellent</span></label>
-                            <input type="radio" name="rate" id="rating-op4" data-idx="1" hidden>
-                            <label for="rating-op4"><span>Very Good </span></label>
-                            <input type="radio" name="rate" id="rating-op3" data-idx="2" hidden>
-                            <label for="rating-op3"><span>Good</span></label>
-                            <input type="radio" name="rate" id="rating-op2" data-idx="3" hidden>
-                            <label for="rating-op2"><span>Good Enough</span></label>
-                            <input type="radio" name="rate" id="rating-op1" data-idx="4" hidden>
-                            <label for="rating-op1"><span>Very Bad</span></label>
-                        </div>
-                    </form>
+bookDetailsContainer.innerHTML = `
+    <div class="row">
+        <div class="col-2">
+            <img src="${book.coverImage}" alt="Book4" width="68%" />
+        </div>
+        <div class="col-2">
+            <p>Home / Ebook</p>
+            <h1>${book.title}</h1>
+            <h4>$${book.price} <i class="fa-solid fa-tags"></i></h4>
+            <input id="quantityInput" min="1" type="number" value="1" />
+            <button class="btn" onclick="addToCart(${`'${book._id}'`})">Add To Cart</button>
+            <div class="btnswq">
+                <button id="btnh112" class="btntr"><i class="fas fa-heart"></i></button>
+                <div class="Cardsa" id="pup">
+                    <img src="../Faver/heart15.png" alt="">
+                    <h1>Add</h1>
+                    <p>To-Favorite</p>
+                    <button class="btncx-close" onclick="hidde_pup()">close</button>
                 </div>
             </div>
-        `;
+            <h3>Book Details <i class="fa fa-indent"></i></h3>
+            <p>${book.description}</p>
+            <a href="#" class="btn" style="margin-left:100px; margin-top: 65px;">Rating</a>
+            <form action="">
+                <div class="rating">
+                    <input type="radio" name="rate" id="rating-op5" data-idx="0" hidden>
+                    <label for="rating-op5"><span>Excellent</span></label>
+                    <input type="radio" name="rate" id="rating-op4" data-idx="1" hidden>
+                    <label for="rating-op4"><span>Very Good </span></label>
+                    <input type="radio" name="rate" id="rating-op3" data-idx="2" hidden>
+                    <label for="rating-op3"><span>Good</span></label>
+                    <input type="radio" name="rate" id="rating-op2" data-idx="3" hidden>
+                    <label for="rating-op2"><span>Good Enough</span></label>
+                    <input type="radio" name="rate" id="rating-op1" data-idx="4" hidden>
+                    <label for="rating-op1"><span>Very Bad</span></label>
+                </div>
+            </form>
+        </div>
+    </div>
+`;
 }
 
 const getBookDetails = async () => {
-    try {
-        const response = await axios.get(`${backendUrl}/product/${selectedBook}`);
-        const bookDetails = response.data.data;
-        bookDetailsRender(bookDetails);
-    } catch (error) {
-        console.log(error);
-    }
+try {
+const response = await axios.get(`${backendUrl}/product/${selectedBook}`);
+const bookDetails = response.data.data;
+bookDetailsRender(bookDetails);
+} catch (error) {
+console.log(error);
+}
 }
 
 const fetchComments = async () => {
-    const response = await fetch(`${backendUrl}/comment/${selectedBook}`);
-    const data = await response.json();
-    const comments = data.data;
-    if (comments) {
-        displayComments(comments);
-    } else {
-        console.error('Comments not found in the response:', data);
-    }
+const response = await fetch(`${backendUrl}/comment/${selectedBook}`);
+const data = await response.json();
+const comments = data.data;
+if (comments) {
+displayComments(comments);
+} else {
+console.error('Comments not found in the response:', data);
+}
 };
 
 const displayComments = (comments) => {
-    commentsContainer.innerHTML = '';
-    comments.forEach(comment => {
-        const commentElement = document.createElement('div');
-        commentElement.classList.add('comment');
-        commentElement.id = comment._id;
-        commentElement.innerHTML = `
-        <div class="comment-header">
-            <strong>${comment.user.username}</strong> - ${formatDate(comment.updatedAt)}
-        </div>
-        <div class="comment-text">${comment.comment}</div>
-        `;
-        commentsContainer.appendChild(commentElement);
-    });
+commentsContainer.innerHTML = '';
+comments.forEach(comment => {
+const commentElement = document.createElement('div');
+commentElement.classList.add('comment');
+commentElement.id = comment._id;
+commentElement.innerHTML = `
+<div class="comment-header">
+    <strong>${comment.user.username}</strong> - ${formatDate(comment.updatedAt)}
+</div>
+<div class="comment-text">${comment.comment}</div>
+`;
+commentsContainer.appendChild(commentElement);
+});
 };
 
 const addComment = async (comment) => {
-    const response = await axios.post(`${backendUrl}/comment`, {
-        comment: comment,
-        product: selectedBook,
-        user: userDetails.user._id
-    })
-    const comments = response.data.data.comments;
-    displayComments(comments);
+const response = await axios.post(`${backendUrl}/comment`, {
+comment: comment,
+product: selectedBook,
+user: userDetails.user._id
+})
+const comments = response.data.data.comments;
+displayComments(comments);
 };
 
 commentForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const newComment = commentInput.value;
-    if (newComment.trim() !== '') {
-        addComment(newComment);
-        commentInput.value = '';
-    }
+event.preventDefault();
+const newComment = commentInput.value;
+if (newComment.trim() !== '') {
+addComment(newComment);
+commentInput.value = '';
+}
 });
+
+const productElementRender = (container, productImage, productId, productTitle, productPrice) => {
+const productElement = document.createElement('div');
+productElement.classList.add('col-4');
+productElement.innerHTML = `
+<a href="../bookDetails.html?id=${productId}">
+<img src="${productImage}" alt="Book 4" /></a>
+<a href="../bookDetails.html?id=${productId}">
+    <h4>${productTitle}</h4>
+</a>
+<div class="rating">
+    <i class="fa fa-star"></i>
+    <i class="fa fa-star"></i>
+    <i class="fa fa-star"></i>
+    <i class="fa fa-star"></i>
+    <i class="fa fa-star-o"></i>
+</div>
+<p>$.${productPrice}</p> 
+`;
+
+container.appendChild(productElement);
+}
+
+
+const getProducts = async (category, page, limit) => {
+productListContainer.innerHTML = '';
+try {
+const response = await axios.get(`${backendUrl}/product?category=${category}&page=${page}&limit=${limit}`);
+const products = response.data.data;
+products.forEach((product) => {
+    productElementRender(productListContainer, product.coverImage, product._id, product.title, product.price);
+});
+} catch (error) {
+console.error(error);
+}
+}
 
 fetchComments();
 getBookDetails();
-
-
-
-
+getProducts('', 1, 4);
--- js/home.js
+++ js/home.js
@@ -1,2 +1,42 @@
 let offerTag = document.querySelector('.offer');
 offerTag.innerHTML = offerContent;
+const productListContainer = document.getElementById('productList');
+
+const productElementRender = (container, productImage, productId, productTitle, productPrice) => {
+    const productElement = document.createElement('div');
+    productElement.classList.add('col-4');
+    productElement.innerHTML = `
+        <a href="../bookDetails.html?id=${productId}">
+        <img src="${productImage}" alt="Book 4" /></a>
+        <a href="../bookDetails.html?id=${productId}">
+            <h4>${productTitle}</h4>
+        </a>
+        <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+        </div>
+        <p>$.${productPrice}</p> 
+    `;
+
+    container.appendChild(productElement);
+}
+
+
+const getProducts = async (category, page, limit) => {
+    productListContainer.innerHTML = '';
+    try {
+        const response = await axios.get(`${backendUrl}/product?category=${category}&page=${page}&limit=${limit}`);
+        const products = response.data.data;
+        products.forEach((product) => {
+            productElementRender(productListContainer, product.coverImage, product._id, product.title, product.price);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+getProducts('', 1, 12);
