7b6a66f1fb4ffe0d932bd61fcee4560905ceee3d
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-12-06 16:44:23 +0200

    handle offer in home page

--- component/offer.js
+++ component/offer.js
@@ -2,19 +2,18 @@ let offerContent = `
 <div class="small-container">
   <div class="row">
     <div class="col-2">
-      <img src="images/offer-Book.jpg" class="offer-img" />
+      <img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1683050173-91aF1JPyq5L.jpg?crop=1xw:1xh;center,top&resize=980:*" width="400px" class="offer-img" />
     </div>
     <div class="col-2">
       <p>Available on EbookStore</p>
       <br />
       <h1>
         <div>
-         
           <h1>Discount Offer <i class="fa-solid fa-tags"></i></h1>
         </div>
 
       </h1>
-      <h2>   I Don't Want To Die Poor</h2>
+      <h2>Romantic Comedy, by Curtis Sittenfeld</h2>
       <br />
       <small>
         Making Michael Arceneaux's I Don't Want to Die Poor required
@@ -22,7 +21,7 @@ let offerContent = `
         young people think twice about the promise that going to college
         at any cost is the only path to upward social mobility.
       </small>
-      <a href="./cart.html" class="btn">Buy Now &#8594;</a>
+      <a href="./cart.html?offer=true" class="btn">Buy Now &#8594;</a>
     </div>
   </div>
 </div>
